b4a4284906afc1c417f7046f8a9454502a9bdc6f
Dylan LANNUZEL <lannuzeld@gmail.com>
2023-05-20 17:06:21 +0200

    Improvements

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,9 @@
 <title>Laravel From Scratch Blog</title>
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+@vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/alpine.js'])
+
 @stack('other-scripts')
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
