ea8e79a8d6ab0af1ed3539770cdcfb3dbec68205
Dylan LANNUZEL <lannuzeld@gmail.com>
2023-05-20 16:00:53 +0200

    Improvements

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/alpine.js'],
             refresh: true,
         }),
         viteStaticCopy({
