944918198e97b4075a2d6c299f5bb7443e79de37
Dylan LANNUZEL <lannuzeld@gmail.com>
2023-08-02 17:20:01 +0200

    New Laravel Prompts implemented on artisan commands

--- app/Console/Commands/CreateAdmin.php
+++ app/Console/Commands/CreateAdmin.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace App\Console\Commands;
 
 use App\Models\User;
+use App\Utilities\Role;
 use Illuminate\Console\Command;
+
+use function Laravel\Prompts\password;
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\text;
 
 final class CreateAdmin extends Command
 {
@@ -38,22 +43,50 @@ final class CreateAdmin extends Command
      */
     public function handle(): int
     {
-        $this->comment('Creating admin user...');
-        $firstname = $this->ask('Firstname');
-        $lastname = $this->ask('Lastname');
-        $username = $this->ask('Username');
-        $email = $this->ask('Email');
-        $password = $this->secret('Password');
-        $confirmPassword = $this->secret('Confirm password');
+        $this->comment('Creating User...');
+        $firstname = text(
+            label: 'What first name do you want?',
+            placeholder: 'E.g. Dylan',
+            required: true,
+        );
+        $lastname = text(
+            label: 'What last name?',
+            placeholder: 'E.g. Lannuzel',
+            required: true,
+        );
+        $username = text(
+            label: 'What username?',
+            placeholder: 'E.g. DiDi',
+            required: true,
+            validate: fn (string $value) => match(true) {
+                User::where('username', $value)->exists() => 'The username is already taken.',
+                default => null,
+            }
+        );
+        $email = text(
+            label: 'What email?',
+            required: true,
+            validate: fn (string $value) => match(true) {
+                false === filter_var($value, FILTER_VALIDATE_EMAIL) => 'The email must be valid.',
+                User::where('email', $value)->exists() => 'The email is already taken.',
+                default => null,
+            }
+        );
+        $role = select(
+            label: 'What role should the user have?',
+            options: Role::toArray(),
+            default: Role::ADMIN->value,
+        );
+        $password = password(
+            label: 'What password?',
+            required: true,
+        );
+        $confirmPassword = password(
+            label: 'Confirm password',
+            required: true,
+        );
         if ($password !== $confirmPassword) {
             $this->error('Passwords do not match!');
-
-            return 1;
-        }
-
-        // Verify if username and email do not already exist
-        if (User::where('username', $username)->orWhere('email', $email)->exists()) {
-            $this->error('User already exists!');
 
             return 1;
         }
@@ -63,10 +96,11 @@ final class CreateAdmin extends Command
         $admin->lastname = $lastname;
         $admin->username = $username;
         $admin->email = $email;
+        $admin->role = $role;
         $admin->password = $password;
         $admin->save();
 
-        $this->info('Admin user created!');
+        $this->info('User created!');
         $admin->sendEmailVerificationNotification();
 
         return 0;
